a91d3e31f8419ba7759ffc7fcc5783b26386f538
Sabir Hussain Balal <246732079+sabirhusseinbalal@users.noreply.github.com>
2026-04-21 17:04:23 +0500

    Fix error message for invalid folder path
    Updated error message for invalid folder path.

--- 07_background_removal/main.py
+++ 07_background_removal/main.py
@@ -79,4 +79,4 @@ while True:
         print(f"Folder Loaded: {path}")
         bg_remover(path)
     else:
-        print("404 - Please enter a valid folder path.")
+        print("Invalid folder path.")
